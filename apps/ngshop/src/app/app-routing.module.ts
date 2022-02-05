70d33bc645e708c979dd5e75d15c6de0180e0618
sabrien96 <sabriensaber0@gmail.com>
2022-02-05 21:43:10 +0200

    back to old design

--- apps/ngshop/src/app/app-routing.module.ts
+++ apps/ngshop/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
+import { RegisterComponent } from './../../../../libs/users/src/lib/pages/register/register.component';
+import { LoginComponent } from './../../../../libs/users/src/lib/pages/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 const routes: Routes = [
   { path: '', component: HomePageComponent },
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
   {
     path: 'user', loadChildren: () => import('./../../../../libs/users/src/lib/user-profile/user-profile.module')
       .then(m => m.UserProfileModule),
   },
+
 
 ];
 
