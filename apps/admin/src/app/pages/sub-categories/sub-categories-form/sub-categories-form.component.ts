d7ad8b5e74fe030ec3705ed3eb9564b8d9af558f
EngMarko110 <88800474+EngMarko110@users.noreply.github.com>
2022-02-11 00:47:37 +0200

    pencil

--- apps/admin/src/app/pages/sub-categories/sub-categories-form/sub-categories-form.component.ts
+++ apps/admin/src/app/pages/sub-categories/sub-categories-form/sub-categories-form.component.ts
@@ -1,12 +1,16 @@
-import { Location } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import { CategoriesService, SubCategory } from '@bluebits/products';
-import { MessageService } from 'primeng/api';
-import { Subject, timer } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-@Component({ selector: 'admin-sub-categories-form', templateUrl: './sub-categories-form.component.html', styles: [] })
+import { Location } from "@angular/common";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute } from "@angular/router";
+import { CategoriesService, SubCategory } from "@bluebits/products";
+import { MessageService } from "primeng/api";
+import { Subject, timer } from "rxjs";
+import { takeUntil } from "rxjs/operators";
+@Component({
+  selector: "admin-sub-categories-form",
+  templateUrl: "./sub-categories-form.component.html",
+  styles: [],
+})
 export class SubCategoriesFormComponent implements OnInit, OnDestroy {
   private endsubs$: Subject<any> = new Subject();
   public form: FormGroup;
@@ -22,7 +26,7 @@ export class SubCategoriesFormComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute
   ) {}
   public ngOnInit(): void {
-    this.form = this.formBuilder.group({ name: ['', Validators.required] });
+    this.form = this.formBuilder.group({ name: ["", Validators.required] });
     this._checkEditMode();
   }
   public ngOnDestroy(): void {
@@ -32,38 +36,87 @@ export class SubCategoriesFormComponent implements OnInit, OnDestroy {
   public onSubmit(): void {
     this.isSubmitted = true;
     if (this.form.invalid) return;
-    const subCategory: SubCategory = { _id: this.currentSubCategoryId, parentCategory: this.currentCategoryId, name: this.subCategoryForm.name.value };
+    const subCategory: SubCategory = {
+      _id: this.currentSubCategoryId,
+      parentCategory: this.currentCategoryId,
+      name: this.subCategoryForm.name.value,
+      icon: "",
+      color: "",
+    };
     if (this.editmode) this._updateSubCategory(subCategory);
     else this._addSubCategory(subCategory);
   }
-  onCancle() { this.location.back(); }
+  onCancle() {
+    this.location.back();
+  }
   private _addSubCategory(subCategory: SubCategory): void {
-    this.categoriesService.createSubCategory(subCategory).pipe(takeUntil(this.endsubs$)).subscribe((subCategory: SubCategory) => {
-      this.messageService.add({ severity: 'success', summary: 'Success', detail: `Sub Category ${subCategory.name} is created!` });
-      timer(2000).toPromise().then(() => this.location.back());
-    }, () => {
-      this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Category is not created!' });
-    });
-  }
-  private _updateSubCategory(subCategory: SubCategory): void {
-    this.categoriesService.updateSubCategory(subCategory).pipe(takeUntil(this.endsubs$)).subscribe(() => {
-      this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Sub Category is updated!' });
-      timer(2000).toPromise().then(() => this.location.back());
-    }, () => this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Sub Category is not updated!' }));
-  }
-  private _checkEditMode(): void {
-    this.route.params.pipe(takeUntil(this.endsubs$)).subscribe((params) => {
-      if (params.parentCategory) {
-        this.currentCategoryId = params.parentCategory;
-        if (params.id) {
-          this.editmode = true;
-          this.currentSubCategoryId = params.id;
-          this.categoriesService.getSubCategory(params.id).pipe(takeUntil(this.endsubs$)).subscribe((subCategory) => {
-            this.subCategoryForm.name.setValue(subCategory.name);
+    this.categoriesService
+      .createSubCategory(subCategory)
+      .pipe(takeUntil(this.endsubs$))
+      .subscribe(
+        (subCategory: SubCategory) => {
+          this.messageService.add({
+            severity: "success",
+            summary: "Success",
+            detail: `Sub Category ${subCategory.name} is created!`,
+          });
+          timer(2000)
+            .toPromise()
+            .then(() => this.location.back());
+        },
+        () => {
+          this.messageService.add({
+            severity: "error",
+            summary: "Error",
+            detail: "Category is not created!",
           });
         }
-      }
-    }, (error) => console.log(error));
+      );
   }
-  get subCategoryForm() { return this.form.controls; }
+  private _updateSubCategory(subCategory: SubCategory): void {
+    this.categoriesService
+      .updateSubCategory(subCategory)
+      .pipe(takeUntil(this.endsubs$))
+      .subscribe(
+        () => {
+          this.messageService.add({
+            severity: "success",
+            summary: "Success",
+            detail: "Sub Category is updated!",
+          });
+          timer(2000)
+            .toPromise()
+            .then(() => this.location.back());
+        },
+        () =>
+          this.messageService.add({
+            severity: "error",
+            summary: "Error",
+            detail: "Sub Category is not updated!",
+          })
+      );
+  }
+  private _checkEditMode(): void {
+    this.route.params.pipe(takeUntil(this.endsubs$)).subscribe(
+      (params) => {
+        if (params.parentCategory) {
+          this.currentCategoryId = params.parentCategory;
+          if (params.id) {
+            this.editmode = true;
+            this.currentSubCategoryId = params.id;
+            this.categoriesService
+              .getSubCategory(params.id)
+              .pipe(takeUntil(this.endsubs$))
+              .subscribe((subCategory) => {
+                this.subCategoryForm.name.setValue(subCategory.name);
+              });
+          }
+        }
+      },
+      (error) => console.log(error)
+    );
+  }
+  get subCategoryForm() {
+    return this.form.controls;
+  }
 }
