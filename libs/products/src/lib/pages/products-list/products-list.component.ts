526677ab9acc14c46c70c80954170694e1e56ed3
EngMarko110 <88800474+EngMarko110@users.noreply.github.com>
2022-02-12 17:19:28 +0200

    h

--- libs/products/src/lib/pages/products-list/products-list.component.ts
+++ libs/products/src/lib/pages/products-list/products-list.component.ts
@@ -1,29 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { param } from 'jquery';
-import { Category } from '../../models/category';
-import { Product } from '../../models/product';
-import { CategoriesService } from '../../services/categories.service';
-import { ProductsService } from '../../services/products.service';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+//import { param } from 'jquery';
+import { Category } from "../../models/category";
+import { Product } from "../../models/product";
+import { CategoriesService } from "../../services/categories.service";
+import { ProductsService } from "../../services/products.service";
 
 @Component({
-  selector: 'products-list',
-  templateUrl: './products-list.component.html',
-  styles: []
+  selector: "products-list",
+  templateUrl: "./products-list.component.html",
+  styles: [],
 })
 export class ProductsListComponent implements OnInit {
   products: Product[] = [];
   categories: Category[] = [];
   isCategoryPage: boolean;
-  subId: any
+  subId: any;
 
   constructor(
     private prodService: ProductsService,
     private catService: CategoriesService,
     private route: ActivatedRoute
-  ) {
-
-  }
+  ) {}
 
   ngOnInit(): void {
     // this.route.params.subscribe((params) => {
@@ -31,19 +29,20 @@ export class ProductsListComponent implements OnInit {
     //   params.categoryid ? (this.isCategoryPage = true) : (this.isCategoryPage = false);
     // });
     this.route.paramMap.subscribe((params) => {
-      this.subId = params.get('subCategoryId');
-      console.log('sub id: ', this.subId);
+      this.subId = params.get("subCategoryId");
+      console.log("sub id: ", this.subId);
     });
     this._getCategories();
     this._getProducts();
   }
 
   private _getProducts() {
-    this.prodService.getProductsBySubCategoryId(this.subId).subscribe((resProducts) => {
-      this.products = resProducts;
-      console.log({resProducts});
-      
-    });
+    this.prodService
+      .getProductsBySubCategoryId(this.subId)
+      .subscribe((resProducts) => {
+        this.products = resProducts;
+        console.log({ resProducts });
+      });
   }
 
   private _getCategories() {
